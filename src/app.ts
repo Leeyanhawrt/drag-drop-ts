9b26ea17302de62b3d558065b6de07f50730a1b6
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-20 00:32:26 -0400

    Add clear value to form after submit

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+// validation (Question mark means that it is optional to add)
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -45,10 +55,37 @@ class ProjectInput {
     this.attachContent();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length
+    ) {
+      alert("Invalid input, please try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.peopleInputElement.value = "";
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput;
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+    }
+    this.clearInputs();
   }
 
   private configure() {
